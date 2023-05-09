74056d0d9f94ac19872ea2a19ebc75cdc444bd14
arifprbdi19 <123190019@student.upnyk.ac.id>
2023-05-10 00:59:30 +0700

    redeploy

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,10 @@ const { defineConfig } = require('@vue/cli-service')
 // const path = require('path');
 module.exports = defineConfig({
   transpileDependencies: true,
-  // devServer: {
-  //   disableHostCheck: true
-  // }
-  publicPath: "https://restaurant-client-dot-if-c-01-project.df.r.appspot.com"
+  devServer: {
+    disableHostCheck: true
+  }
+  // publicPath: "https://restaurant-client-dot-if-c-01-project.df.r.appspot.com"
 });
 
 // module.exports = {

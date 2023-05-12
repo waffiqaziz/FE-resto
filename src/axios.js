10db77a26413ac4fec9af5a196c8d39bca492bc0
arifprbdi19 <123190019@student.upnyk.ac.id>
2023-05-13 01:15:25 +0700

    build

--- src/axios.js
+++ src/axios.js
@@ -3,5 +3,5 @@ import axios from "axios";
 window.axios = axios
 axios.defaults.withCredentials = false
 // axios.defaults.baseURL = "http://localhost:8081/api"
-let backendUrl = "http://" + window.location.hostname.toString() + ":8081/api"
+let backendUrl = "https://restaurant-server-dot-if-c-01-project.df.r.appspot.com/api"
 axios.defaults.baseURL = backendUrl
